185a1fb3fcf8be40c480c3af46535b1c9c5bd700
riccdigital <eryc.mabonga@gmail.com>
2021-01-31 21:16:55 +0300

    input output variable declaration

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,5 +1,9 @@
 window.onload = computerPlay()
+const input = document.getElementById('player-input')
+const output = document.getElementById('output')
+const btn = document.getElementById ('btn')
 
+const playerSelection = 0
 function computerPlay () {
   const selection = ['rock', 'paper', 'scissors']
   const computerSelection = selection[Math.floor(Math.random() * selection.length)]

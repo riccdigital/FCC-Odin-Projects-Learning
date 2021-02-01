80f625d6590a2491f26d9f968c5b9e1bfcfead13
riccdigital <eryc.mabonga@gmail.com>
2021-02-01 09:12:47 +0300

    introduced playerValue variable to represent playerselection.tolowercase

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -14,33 +14,34 @@ function computerPlay () {
   const selection = ['rock', 'paper', 'scissors']
   const computerSelection = selection[Math.floor(Math.random() * selection.length)]
   const playerSelection = document.getElementById('player-input').value
+  const playerValue = playerSelection.toLowerCase()
 
   if (computerSelection === 'rock') {
-    if (playerSelection.toLowerCase() === 'rock') {
+    if (playerValue === 'rock') {
       output.innerHTML = 'Computer also chose Rock, Play Again'
-    } else if (playerSelection.toLowerCase() === 'paper') {
+    } else if (playerValue === 'paper') {
       output.innerHTML = 'Computer chose Rock, You Win'
-    } else if (playerSelection.toLowerCase() === 'scissors') {
+    } else if (playerValue === 'scissors') {
       output.innerHTML = 'Computer also chose Rock, Lose'
     } else {
       output.innerHTML = 'Please make a valid choice'
     }
   } else if (computerSelection === 'paper') {
-    if (playerSelection.toLowerCase() === 'paper') {
+    if (playerValue === 'paper') {
       output.innerHTML = 'Computer also chose Paper, Play Again'
-    } else if (playerSelection.toLowerCase() === 'scissors') {
+    } else if (playerValue === 'scissors') {
       output.innerHTML = 'Computer chose Paper, You Win'
-    } else if (playerSelection.toLowerCase() === 'rock') {
+    } else if (playerValue === 'rock') {
       output.innerHTML = 'Computer chose Paper, You Lose'
     } else {
       output.innerHTML = 'Please make a valid choice'
     }
   } else {
-    if (playerSelection.toLowerCase() === 'scissors') {
+    if (playerValue === 'scissors') {
       output.innerHTML = 'Computer also chose Scissors, Play Again'
-    } else if (playerSelection.toLowerCase() === 'rock') {
+    } else if (playerValue === 'rock') {
       output.innerHTML = 'Computer chose Scissors, You Win'
-    } else if (playerSelection.toLowerCase() === 'paper') {
+    } else if (playerValue === 'paper') {
       output.innerHTML = 'Computer chose Scissors, You Lose'
     } else {
       output.innerHTML = 'Please make a valid choice'
